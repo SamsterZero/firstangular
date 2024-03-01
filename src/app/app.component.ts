d61aa1229cd9df22fe7414fa03ce24401d13ca9d
Vinay Mhatre <90505086+SamsterZero@users.noreply.github.com>
2024-03-01 17:28:03 +0530

    Added routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-
-// @Component({
-//   selector: 'app-root',
-//   standalone: true,
-//   imports: [RouterOutlet],
-//   templateUrl: './app.component.html',
-//   styleUrl: './app.component.css'
-// })
+import {Component} from '@angular/core';
+import {HomeComponent} from './home/home.component';
+import {RouterLink, RouterOutlet} from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, RouterLink, RouterOutlet],
   template: `
     <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
-      </header>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+        </header>
+      </a>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Home';
+  title = 'homes';
 }
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,10 +1,11 @@
 import {Component, Input} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HousingLocation} from '../housinglocation';
+import {RouterModule} from '@angular/router';
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   template: `
     <section class="listing">
       <img
@@ -15,6 +16,7 @@ import {HousingLocation} from '../housinglocation';
       />
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
       <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
   `,
   styleUrls: ['./housing-location.component.css'],
